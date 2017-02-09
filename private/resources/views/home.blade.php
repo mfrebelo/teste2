33266eb0bc302b739a2feb45a40aa2fd02fc0d02
mfr <miguelfrebelo@gmail.com>
2017-02-09 01:00:03 +0000

    created migration changed db server to mysql created Link download libraries(bower) edited home.blade

--- private/resources/views/home.blade.php
+++ private/resources/views/home.blade.php
@@ -2,8 +2,30 @@
 
 @section ('content')
 
-  <h1 align=center>@lang('admin.WELCOME_1')</h1>
-  <br><br>
-  <p align=center>@lang('admin.WELCOME_2')</p>
+    <div class="box box-solid box-primary">
+        <div class="box-header with-border">
+            <h3 class="box-title">Links</h3>
+            <div class="box-tools pull-right">
+                <!-- Buttons, labels, and many other things can be placed here! -->
+                <!-- Here is a label for example -->
+                <span class="label label-primary">Links</span>
+            </div><!-- /.box-tools -->
+        </div><!-- /.box-header -->
+        <div class="box-body">
+            @foreach ($links as $link)
+                <h4> Title</h4>
+                <p>{{ $link->title }}</p>
+                <h4> Url</h4>
+                <p>{{ $link->url}}</p>
+                <h4> Description</h4>
+                <p>{{ $link->description}}</p>
+                <hr>
+            @endforeach
+        </div><!-- /.box-body -->
+        <div class="box-footer">
+            The footer of the box
+        </div><!-- box-footer -->
+    </div><!-- /.box -->
+
 
 @stop
